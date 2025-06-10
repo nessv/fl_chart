20b9bbc24979b7758d09ef2dd179be7b9d023bd3
Néstor Valdez <nvaldez@powerley.com>
2025-06-10 12:22:44 -0300

    Adds support for a hatch pattern for the bar chart rods

--- lib/src/chart/bar_chart/bar_chart_data.dart
+++ lib/src/chart/bar_chart/bar_chart_data.dart
@@ -341,6 +341,8 @@ class BarChartRodData with EquatableMixin {
     BorderSide? borderSide,
     BackgroundBarChartRodData? backDrawRodData,
     List<BarChartRodStackItem>? rodStackItems,
+    this.useHatchPattern = false,
+    this.hatchColor,
   })  : fromY = fromY ?? 0,
         color =
             color ?? ((color == null && gradient == null) ? Colors.cyan : null),
@@ -399,6 +401,12 @@ class BarChartRodData with EquatableMixin {
   /// Determines the upward or downward direction
   bool isUpward() => toY >= fromY;
 
+  /// If you want to have a forward hatch pattern, set this value.
+  final bool useHatchPattern;
+
+  /// The color for the forward hatch pattern
+  final Color? hatchColor;
+
   /// Copies current [BarChartRodData] to a new [BarChartRodData],
   /// and replaces provided values.
   BarChartRodData copyWith({
@@ -413,6 +421,8 @@ class BarChartRodData with EquatableMixin {
     BorderSide? borderSide,
     BackgroundBarChartRodData? backDrawRodData,
     List<BarChartRodStackItem>? rodStackItems,
+    bool? useHatchPattern,
+    Color? hatchColor,
   }) =>
       BarChartRodData(
         fromY: fromY ?? this.fromY,
@@ -426,6 +436,8 @@ class BarChartRodData with EquatableMixin {
         borderSide: borderSide ?? this.borderSide,
         backDrawRodData: backDrawRodData ?? this.backDrawRodData,
         rodStackItems: rodStackItems ?? this.rodStackItems,
+        useHatchPattern: useHatchPattern ?? this.useHatchPattern,
+        hatchColor: hatchColor ?? this.hatchColor,
       );
 
   /// Lerps a [BarChartRodData] based on [t] value, check [Tween.lerp].
